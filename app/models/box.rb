95ffea58e37fd18d9053099f5c87866b75e32799
shoko furushita <shktkt219@gmail.com>
2016-10-03 16:58:02 -0400

    add nested attributes for items to box

--- app/models/box.rb
+++ app/models/box.rb
@@ -3,8 +3,12 @@ class Box < ActiveRecord::Base
   has_many :box_items
   has_many :items, through: :box_items
 
-  validates :title, :plan_id, presence: true
-
+  validates :title, :month_year, :plan_id, presence: true
+  accepts_nested_attributes_for :items, reject_if: :reject_items
   default_scope { order(created_at: :desc) }
+
+  def reject_items(attributes)
+    attributes['title'].blank? && attributes['size'].blank?
+  end
 
 end
