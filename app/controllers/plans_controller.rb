5559bde619eb575ae4fb33a829af9429bdda0f90
shoko furushita <shktkt219@gmail.com>
2016-10-15 22:47:19 -0400

    add plans controller new action

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,4 +1,5 @@
 class PlansController < ApplicationController
+
   def index
     @plans = Plan.all
   end
@@ -7,4 +8,26 @@ class PlansController < ApplicationController
     @plan = Plan.find(params[:id])
     @boxes = @plan.boxes
   end
+
+  def new
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+    if @plan.save
+      flash[:notice] = "Successfully created"
+      redirect_to plans_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:name, :price, :description)
+  end
+
+
 end
