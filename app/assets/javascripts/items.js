cd38f8e641ee74a7dac1738fe3a83f42dc59e26f
shoko furushita <shktkt219@gmail.com>
2016-10-18 15:57:22 -0400

    edit items.js and add div items new views

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,14 +1,20 @@
 $(function(){
-  $("form#new_item").on("submit", function(e){
-    e.preventDefault();
-
+  $("form#new_item").on("submit", function(event){
+    event.preventDefault();
     var $form = $(this);
     var action = $form.attr("action")
     var params = $form.serialize()
 
-    $.post(action, params)
+    $.ajax({
+      url: action,
+      data: params,
+      dataType: "json",
+      method: "POST"
+    })
     .success(function(json){
-      console.log(json)
+      var item = new Item(json);
+
+      $("div.item").append(html);
     })
     .error(function(response){
       console.log("You broke it?", response)
