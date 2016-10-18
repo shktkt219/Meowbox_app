b4a19d678bfc579f6012294db2b47627df2eae23
shoko furushita <shktkt219@gmail.com>
2016-10-18 17:10:16 -0400

    add prototype to items.js and edit items new view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require handlebars
+//= require handlebars.min
 //= require_tree .
--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,21 +1,30 @@
-function Item(attributes){
-  this.item_name = attributes.item_name;
-  this.description = attributes.description;
-  this.image = attributes.image;
-  this.url = attributes.url;
-  this.size = attributes.size;
-}
+function Item(json){
+  this.item_name = json.item_name;
+  this.description = json.description;
+  this.image = json.image;
+  this.url = json.url;
+  this.size = json.size;
+};
 
-Item.prototype.renderLI = function(){
-  
-}
+Item.prototype.renderItem = function(){
+  var my_html = ""
+  my_html += '    <div>';
+  my_html += '      <h3>'+ this.item_name + '</h3>';
+  my_html += '      <p>' + this.description + '</p>';
+  my_html += '      <p>' + this.image + '</p>';
+  my_html += '      <p>' + this.size + '</p>';
+  my_html += '      <p>' + this.url + '</p>';
+  my_html += '    </div>';
+
+  return my_html;
+};
 
 $(function(){
   $("form#new_item").on("submit", function(event){
     event.preventDefault();
     var $form = $(this);
-    var action = $form.attr("action")
-    var params = $form.serialize()
+    var action = $form.attr("action");
+    var params = $form.serialize();
 
     $.ajax({
       url: action,
@@ -25,9 +34,9 @@ $(function(){
     })
     .success(function(json){
       var item = new Item(json);
-      var itemLi = item.renderLI()
+      var itemDisplay = item.renderItem()
 
-      $("div.item").append(itemLi);
+      $("div.item").append(itemDisplay);
     })
     .error(function(response){
       console.log("You broke it?", response)
