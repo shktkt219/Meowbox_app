5aed9ac5c52a1c88abf79576044178317c76abf7
shoko furushita <shktkt219@gmail.com>
2016-10-19 01:38:08 -0400

    add hundlebars code to items.js

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,45 +1,32 @@
-function Item(json){
-  this.item_name = json.item_name;
-  this.description = json.description;
-  this.image = json.image;
-  this.url = json.url;
-  this.size = json.size;
-};
-
-Item.prototype.renderItem = function(){
-  var my_html = ""
-  my_html += '    <div>';
-  my_html += '      <h3>'+ this.item_name + '</h3>';
-  my_html += '      <p>' + this.description + '</p>';
-  my_html += '      <p>' + this.image + '</p>';
-  my_html += '      <p>' + this.size + '</p>';
-  my_html += '      <p>' + this.url + '</p>';
-  my_html += '    </div>';
-
-  return my_html;
+function Item(item){
+  this.item_name = item.item_name;
+  this.description = item.description;
+  this.image = item.image;
+  this.size = item.size;
+  this.url = item.url;
 };
 
 $(function(){
-  $("form#new_item").on("submit", function(event){
-    event.preventDefault();
-    var $form = $(this);
-    var action = $form.attr("action");
-    var params = $form.serialize();
-
-    $.ajax({
-      url: action,
-      data: params,
-      dataType: "json",
-      method: "POST"
-    })
-    .success(function(json){
-      var item = new Item(json);
-      var itemDisplay = item.renderItem()
-
-      $("div.item").append(itemDisplay);
-    })
-    .error(function(response){
-      console.log("You broke it?", response)
-    })
-  })
+  Item.templateSource = $("#item-template").html()
+  Item.template = Handlebars.compile(Item.templateSource);
 })
+
+
+Item.prototype.renderLI = function(){
+  return Item.template(this);
+};
+
+$(function(){
+  $("form#new_item").submit(function(event){
+    event.preventDefault();
+    var values = $(this).serialize();
+    var posting = $.post('/items', values);
+
+    posting.done(function(data){
+      var item = data["item"];
+      var newItem = new Item(item);
+      var itemDisplay = newItem.renderLI()
+      $("div.new_created_items").append(itemDisplay)
+    });
+  });
+});
